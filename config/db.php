ec3fa11f58bd1397b0201ba6a2943b1686f01536
Yauheni Ramankou <iromankov2010@gmail.com>
2024-06-22 17:26:43 +0300

    chore: add option to db config

--- config/db.php
+++ config/db.php
@@ -8,5 +8,6 @@ return [
     'charset'=> 'utf8mb4', // utf8mb4
     'options' => [
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        //PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // php < v.8
     ],
 ];
